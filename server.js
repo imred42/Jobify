f762febfee4d71101be3a44af19072d16489b100
Chenfei <chenfei.xiong@outlook.com>
2024-09-12 20:09:55 +0200

    Revert "fix bug"
    This reverts commit fcf50b63fbda3628d4e49d767c7d7b8024f0d959.

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import path from 'path';
 const __dirname = dirname(fileURLToPath(import.meta.url));
-app.use(express.static(path.resolve(__dirname, './client/dist')));
+app.use(express.static(path.resolve(__dirname, './public')));
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
@@ -34,7 +34,7 @@ app.use("/api/v1/jobs", authenticateUser, jobRouter);
 app.use("/api/v1/auth", authRouter);
 app.use('/api/v1/users', authenticateUser, userRouter);
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
+  res.sendFile(path.resolve(__dirname, './public', 'index.html'));
 });
 
 app.get('/api/v1/test', (req, res) => {
